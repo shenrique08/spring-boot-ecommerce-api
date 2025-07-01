8c47325f8e2763bd3390a92c8a029094a7df6712
Sergio Henrique <114961377+shenrique08@users.noreply.github.com>
2025-07-01 18:21:46 -0300

    adding the initial project

---
+++ src/test/java/org/springbootapi/api/SpringbootapiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springbootapi.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
