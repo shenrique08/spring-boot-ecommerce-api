c1d62db84ff270aab900a530b1d8a44356007b42
Sergio Henrique <sergimmatos@hotmail.com>
2025-07-02 18:24:16 -0300

    Adding the Service layer and the component registration

--- src/main/java/org/springbootapi/api/resources/UserResource.java
+++ src/main/java/org/springbootapi/api/resources/UserResource.java
@@ -1,23 +1,39 @@
 package org.springbootapi.api.resources;
 
 import org.springbootapi.api.entities.User;
+import org.springbootapi.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+
+    private final UserService service;
+
+    @Autowired
+    public UserResource(UserService service) {
+        this.service = service;
+    }
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User userExample = User.builder()
-                .name("Maria")
-                .email("maria@example.com")
-                .phone("999999")
-                .password("43554")
-                .build();
-        return ResponseEntity.ok().body(userExample);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> userList = service.findAll();
+
+        return ResponseEntity.ok().body(userList);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        return service.findById(id)
+                .map(user -> ResponseEntity.ok().body(user))
+                .orElse(ResponseEntity.notFound().build());
     }
 }
---
+++ src/main/java/org/springbootapi/api/services/UserService.java
@@ -0,0 +1,29 @@
+package org.springbootapi.api.services;
+
+
+import org.springbootapi.api.entities.User;
+import org.springbootapi.api.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository repository;
+
+    @Autowired
+    public UserService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public Optional<User> findById(Long id) {
+        return repository.findById(id);
+    }
+}
