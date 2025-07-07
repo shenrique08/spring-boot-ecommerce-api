61dd2b36e82675964a8722c963b700ee0034b540
Sergio Henrique <sergimmatos@hotmail.com>
2025-07-07 18:52:28 -0300

    Implementing User update

--- src/main/java/org/springbootapi/api/entities/User.java
+++ src/main/java/org/springbootapi/api/entities/User.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 import java.io.Serializable;
@@ -12,6 +13,7 @@ import java.util.List;
 import java.util.Objects;
 
 @Getter
+@Setter
 @ToString
 @Entity
 @Table(name = "tb_user")
