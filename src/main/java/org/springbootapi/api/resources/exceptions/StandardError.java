e922f33d4adb1fb9893da745d2857051ca0ac7a3
Sergio Henrique <sergimmatos@hotmail.com>
2025-07-08 19:28:32 -0300

    Exception handling

---
+++ src/main/java/org/springbootapi/api/resources/exceptions/StandardError.java
@@ -0,0 +1,33 @@
+package org.springbootapi.api.resources.exceptions;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+
+@Getter
+@Setter
+public class StandardError implements Serializable {
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
+    private Instant timestamp;
+    private Integer status;
+    private String error;
+    private String message;
+    private String path;
+
+    public StandardError() {}
+
+    @Builder
+    public StandardError(Instant timestamp, Integer status, String error, String message, String path) {
+        this.timestamp = timestamp;
+        this.status = status;
+        this.error = error;
+        this.message = message;
+        this.path = path;
+    }
+}
---
+++ src/main/java/org/springbootapi/api/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package org.springbootapi.api.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(Object id) {
+        super("Resource not found. Id " + id);
+    }
+}
