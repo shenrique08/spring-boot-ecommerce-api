3138314af9c1a7f644c5ad0c54b9bd0df8406d35
Sergio Henrique <sergimmatos@hotmail.com>
2025-07-18 14:31:20 -0300

    feat: Initial project structure

---
+++ src/main/java/org/api/springbootapiumlcase/SpringbootApiUmlCaseApplication.java
@@ -0,0 +1,13 @@
+package org.api.springbootapiumlcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApiUmlCaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApiUmlCaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/api/springbootapiumlcase/SpringbootApiUmlCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.api.springbootapiumlcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApiUmlCaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
