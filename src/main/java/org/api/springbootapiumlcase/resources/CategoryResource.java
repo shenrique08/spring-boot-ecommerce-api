e2e6c82e93d27dc6407d3f5e181b851d990456f2
Sergio Henrique <sergimmatos@hotmail.com>
2025-07-18 14:38:45 -0300

    REST testing

---
+++ src/main/java/org/api/springbootapiumlcase/resources/CategoryResource.java
@@ -0,0 +1,16 @@
+package org.api.springbootapiumlcase.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+
+    @GetMapping
+    public String list() {
+        return "Rest is working";
+    }
+
+}
